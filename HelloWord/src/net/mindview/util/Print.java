4c68933cdec8b04baf5280da888d665f1173f323
papertiger <papertiger8848@gmail.com>
2013-10-03 15:38:54 +0800

    Study "Think in Java" series (Access Control)

---
+++ HelloWord/src/net/mindview/util/Print.java
@@ -0,0 +1,37 @@
+//:net/mindview/util/Print.java
+//Print methods that can be used without
+//qualifiers, using Java SE5 static imports:
+
+package net.mindview.util;
+
+import java.io.PrintStream;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: papertiger
+ * Date: 10/3/13
+ * Time: 3:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Print {
+    //Print with a newline:
+    public static void print(Object obj) {
+        System.out.println(obj);
+    }
+
+    //Print a newline by itself:
+    public static void print() {
+        System.out.println();
+    }
+
+    //Print with no line break:
+    public static void printnb(Object obj) {
+        System.out.println(obj);
+    }
+
+    //The new Java SE5 print()(from C):
+    public static PrintStream
+    printf(String format, Object ... args) {
+        return System.out.printf(format, args);
+    }
+} ///:~
